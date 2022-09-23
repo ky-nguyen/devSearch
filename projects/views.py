80d0dc3d6ba8f0fa268c100d9f1fb797f93b9dea
ky-nguyen <kynguyen00710011999@gmail.com>
2022-09-24 01:07:28 +0700

    Add data on database

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Project
 
 
 projectsList = [
@@ -21,19 +21,13 @@ projectsList = [
     },
 ]
 def projects(request):
-    page = 'projects'
-    number = 10
+    projects = Project.objects.all()
     context = {
-        'page': page,
-        'number': number,
-        'projects': projectsList
+        'projects': projects,
     }
     return render(request, 'projects/projects.html', context)
 
 
 def project(request, pk):
-    projectObj = None
-    for i in projectsList:
-        if i['id'] == pk:
-            projectObj = i
+    projectObj = Project.objects.get(id=pk)
     return render(request, 'projects/single-project.html', {'project': projectObj})
